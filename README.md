db9c9b3c80613af237da846ecc2dd1890fa0f22e
Moguf <kbu94985@gmail.com>
2016-08-08 16:47:51 +0900

    INIT: add fundamental dirs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 2DIsing_simulation
+Simulating 2D-Ising with GPU.
+
+## Reuirements
+- CUDA Compiler >= 7.5
+- Gradle  >= 2.10
+
+## Build
+    gradle
+
+## Run
---
+++ src/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main_generated_2d_ising.cu.o"
+  "../bin/main.pdb"
+  "../bin/main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
